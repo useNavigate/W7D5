e66b6fcf034f8eaa8f02b54ec7908537ddc59dd9
Sean Abbas <sean.abbas33@gmail.com>
2023-02-24 12:18:19 -0500

    Sign in valid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["Invalid credentials. Please try again cuh"]
+      @username = params[:user][:username] # params is what we put in the form
+      render :new
+    end
 
   end
 
